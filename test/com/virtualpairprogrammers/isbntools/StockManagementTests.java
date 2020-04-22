d15b98f9b3bb2c800e81e128d94e945874e81049
Maria Bilous <maria.bilous101@gmail.com>
2020-04-22 14:33:20 +0300

    optimize repetitions, use @Before annotation

--- test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
+++ test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -1,11 +1,29 @@
 package com.virtualpairprogrammers.isbntools;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
+    ExternalISBNDataService testWebService;
+    ExternalISBNDataService testDatabaseService;
+    StockManager stockManager;
+
+    //annotation @Before tells JUnit that this method to be run prior to each test.
+    //if something is being instantiating here, there will be a "fresh" version prior to each test
+    //we can use an object from setup() in any tests.
+    @Before
+    public void setup() {
+        //instead of instantiating (and implementing) an object as in the first test, we ask Mockito to do it for us
+        //at this point we have got a fake(or a dummy) as we have not had implementation so far:
+        testWebService = mock(ExternalISBNDataService.class);
+        testDatabaseService = mock(ExternalISBNDataService.class);
+        stockManager = new StockManager();
+        stockManager.setWebService(testWebService);
+        stockManager.setDataBaseService(testDatabaseService);
+    }
 
     //We are creating a TEST STUB (replacement for an object that the clause we are testing has a dependency on):
 
@@ -42,18 +60,9 @@ public class StockManagementTests {
     public void testCanGetACorrectLocatorCodeUsingMocks() {
         String isbn = "0140177396";
 
-        //instead of instantiating (and implementing) an object as in the above test, we ask Mockito to do it for us
-        //at this point we have got a fake(or a dummy) as we have not had implementation so far:
-        ExternalISBNDataService testWebService = mock(ExternalISBNDataService.class);
-        ExternalISBNDataService testDatabaseService = mock(ExternalISBNDataService.class);
-
-        //here we give implementation to our previously constructed mock:
+        //here we give implementation to our constructed mocks constructed in the setup method:
         when(testWebService.lookup(anyString())).thenReturn(new Book(isbn, "Of Mice And Men", "J. Steinbeck"));
         when(testDatabaseService.lookup(anyString())).thenReturn(null);
-
-        StockManager stockManager = new StockManager();
-        stockManager.setWebService(testWebService);
-        stockManager.setDataBaseService(testDatabaseService);
 
         String locatorCode = stockManager.getLocatorCode(isbn);
 
@@ -64,43 +73,26 @@ public class StockManagementTests {
     public void databaseIsUsedIfDataIsPresent() {
         String isbn = "0140177396";
 
-        //Mockito helps us to create a dummy class that is an implementation of our interface
-        //(has default implementation of our interface)
-        //that we can supply into our object (stockManager) and we can call its methods.
-        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
-        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
-
         //here we do not care what is returned, but we are interested whether lookup() is actually get called.
-        when(databaseService.lookup(isbn)).thenReturn(new Book("0140177396", "abc", "abc"));
-
-        StockManager stockManager = new StockManager();
-        stockManager.setWebService(webService);
-        stockManager.setDataBaseService(databaseService);
+        when(testDatabaseService.lookup(isbn)).thenReturn(new Book("0140177396", "abc", "abc"));
 
         String locatorCode = stockManager.getLocatorCode(isbn);
 
         //we do not care what the method lookup(isbn) returns, but whether it is called by databaseService one time
-        verify(databaseService).lookup(isbn);
-        verify(webService, never()).lookup(anyString());
+        verify(testDatabaseService).lookup(isbn);
+        verify(testWebService, never()).lookup(anyString());
     }
 
     @Test
     public void webServiceIsUsedIfDataIsNotPresentInDatabase() {
         String isbn = "0140177396";
 
-        ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
-        ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
-
-        when(databaseService.lookup(isbn)).thenReturn(null);
-        when(webService.lookup(isbn)).thenReturn(new Book("0140177396", "abc", "abc"));
-
-        StockManager stockManager = new StockManager();
-        stockManager.setWebService(webService);
-        stockManager.setDataBaseService(databaseService);
+        when(testDatabaseService.lookup(isbn)).thenReturn(null);
+        when(testWebService.lookup(isbn)).thenReturn(new Book("0140177396", "abc", "abc"));
 
         String locatorCode = stockManager.getLocatorCode(isbn);
 
-        verify(databaseService).lookup(isbn);
-        verify(webService).lookup(anyString());
+        verify(testDatabaseService).lookup(isbn);
+        verify(testWebService).lookup(anyString());
     }
 }
